271a97aabb3fe9119d1eeca0fac221ce72fb56f2
Cameron Stapp <cstapp5@wgu.edu>
2022-10-25 05:47:01 +0000

    Logging user log-ins added.

--- LoginForm.cs
+++ LoginForm.cs
@@ -30,9 +30,9 @@ namespace Appointment_App
         {
             if (Logic.VerifyUser(userNameLoginTextBox.Text, passwordLoginTextBox.Text) != 0)
             {
-                using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, "logins.txt"), true))
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(path), true))
                 {
-                    outputFile.WriteLine($"{Logic.CurrentUserName} logged in at {Logic.Now}");
+                    outputFile.WriteLine($"{Logic.CurrentUserName} logged in --- {Logic.GetDateTime()}");
                 }
 
                 MessageBox.Show($"Hello {Logic.CurrentUserName}, Sign-in Successful");
